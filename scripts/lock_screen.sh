76989442bd63ef14991fa282c3f51e793a314cd2
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-09-18 11:51:27 -0500

    Changed screen locker for `xsecurelock`

--- scripts/lock_screen.sh
+++ scripts/lock_screen.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [[ -n $@ ]]
+if [[ -z $@ ]]
 then
-    for (( i=5 ; i>0 ; --i )); do echo "Tu pantalla se apagará en $i."; sleep 1; done | dzen2 -x 660 -y 490 -w 600 -h 100 -fn "xos4 Terminus:size=30" -e 'button1=exec:killall lock_screen'
+    for (( i=5 ; i>0 ; --i )); do echo "Tu pantalla se apagará en $i."; sleep 1; done | dzen2 -x 660 -y 490 -w 600 -h 100 -fn "xos4 Terminus:size=30" -e 'button1=exec:killall lock_screen.sh'
 fi
 
-i3lock-fancy -n -b=0x5
+export XSECURELOCK_PASSWORD_PROMPT=disco
+xsecurelock
