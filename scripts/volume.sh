756762b6cf73d781925bd5d3ec9954226d0da418
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-09-18 11:54:01 -0500

    Enable not active sound managment.

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,4 +1,5 @@
 #!/bin/env sh
 
-SOUND=`pactl list sinks short | grep "RUNNING" | sed -n '1p' | awk '{printf "%s", $2}'`
+SOUND=$(pactl list sinks short | awk '{printf "%s %s\n", $7, $2}' | sort |
+        head -1 | awk '{printf "%s\n", $2}')
 pactl set-sink-volume $SOUND $@
