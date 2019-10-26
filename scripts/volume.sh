102fea1ffed9e1fca1f40c7cd044a5d6444aa08b
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-10-25 23:14:31 -0500

    Finetuning? bluetooth connections information

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
 
-SOUND=$(pactl list sinks short | awk '{printf "%s %s\n", $7, $2}' | sort |
+SOUND=$(pactl list sinks short | awk '{printf "%s %s\n", $7, $2}' | sort -r|
         head -1 | awk '{printf "%s\n", $2}')
 pactl set-sink-volume $SOUND $@
