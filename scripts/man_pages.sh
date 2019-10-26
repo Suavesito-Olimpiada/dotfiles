b922729b01c48ea78804868f58cef361221f6594
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-10-25 23:13:51 -0500

    Change st to termite

--- scripts/man_pages.sh
+++ scripts/man_pages.sh
@@ -7,5 +7,5 @@
 search=$(man -k . | dmenu -i -l 11 -p "Man Page: " | awk '{print $2 " " $1}' | sed 's/(\(.*\))\(.*\)/\1\2/')
 if [[ -n $search ]]
 then
-    st -e bash -ci "man $search"
+    termite -e "bash -ci \"man $search\""
 fi
--- scripts/manv_pages.sh
+++ scripts/manv_pages.sh
@@ -3,5 +3,5 @@
 search=$(man -k . | dmenu -i -l 11 -p "Man Page: " | awk '{print $2 " " $1}' | sed 's/(\(.*\))\(.*\)/\1\2/')
 if [[ -n $search ]]
 then
-    st -e bash -ci "man $search | env MAN_PN=1 vim -M +MANPAGER -"
+    termite -e "bash -ci \"man $search | env MAN_PN=1 vim -M +MANPAGER -\""
 fi
