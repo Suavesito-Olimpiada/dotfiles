0a45028c96e945564c5ab7f5bc4a8b70701a1996
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-10-27 13:19:36 -0600

    Correcting syntax error and changing bash for sh

--- scripts/cronbat.sh
+++ scripts/cronbat.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/sh
 # Notify me with notify-send if my battery is below $@%.
 # You can set this to run via cron.
 
@@ -6,10 +6,10 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 BAT=$(cat /sys/class/power_supply/BAT1/capacity)
 if [[ -f /sys/class/power_supply/BAT1/capacity ]]
 then
-    if (( $BAT < $@ ))
+    if (( $BAT -le $@ ))
     then
         notify-send -u critical "Battery critically low."
-    elif (( $BAT >= 98 ))
+    elif (( $BAT -ge 98 ))
     then
         notify-send -u critical "Battery completely charged."
     fi
