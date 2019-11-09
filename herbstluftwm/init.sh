d1364c383b7b5459c959c625614810a6ea479413
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-11-08 21:53:31 -0600

    Changed yaourt for pacman\
    Also change /home/jose for ~

--- herbstluftwm/init.sh
+++ herbstluftwm/init.sh
@@ -2,16 +2,17 @@
 
 xset 120 5
 xset r rate 300 25
-xss-lock -l -- '/home/jose/.config/script/lock_screen' &!
+xss-lock -l -- '~/.config/script/lock_screen' &!
 autocutsel -fork &!
 autocutsel -selection PRIMARY -fork &! #Makes cut and paste behave as expected
 mpd &> /dev/null &!
-dunst -config /home/jose/.config/dunst/dunstrc &!
+/usr/lib/kdeconnectd&!
+dunst -config '~/.config/dunst/dunstrc' &!
 # /usr/lib/notify-osd/notify-osd &!
 # blueman-applet &!
-compton --config /home/jose/.config/compton/compton.conf &!
-/home/jose/Apps/precompile/lifecalendar/lifecalendar.py -c /home/jose/Apps/precompile/lifecalendar/examples/default.cfg -o "/home/jose/Apps/precompile/lifecalendar/BgLifetime.png"
-feh --bg-fill --no-fehbg /home/jose/Apps/precompile/lifecalendar/BgLifetime.png
+compton --config '~/.config/compton/compton.conf' &!
+'~/Apps/precompile/lifecalendar/lifecalendar.py' -c '~/Apps/precompile/lifecalendar/examples/default.cfg' -o '~/Apps/precompile/lifecalendar/BgLifetime.png'
+feh --bg-fill --no-fehbg '~/Apps/precompile/lifecalendar/BgLifetime.png'
 setxkbmap latam
 synclient TapButton1=1
 synclient TapButton2=3
@@ -19,11 +20,11 @@ synclient TapButton3=2
 synclient PalmDetect=1
 numlockx on
 
-/home/jose/.config/herbstluftwm/panel_var.sh &!
+'~/.config/herbstluftwm/panel_var.sh' &!
 
-cd /home/jose/.dotfiles/installed/
+cd '~/.dotfiles/installed/'
 
-yaourt -Q --date > packages.system
+pacman -Q > packages.system
 pip2 freeze --local > py2ckages.system
 pip3 freeze --local > py3ckages.system
 gem list > gemckages.system
