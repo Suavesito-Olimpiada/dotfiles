780ad7c608d374bbeca51b73752a7a6095c80d34
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-11-08 22:05:58 -0600

    Correct scripts

--- scripts/cronbat.sh
+++ scripts/cronbat.sh
@@ -6,10 +6,10 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 BAT=$(cat /sys/class/power_supply/BAT1/capacity)
 if [[ -f /sys/class/power_supply/BAT1/capacity ]]
 then
-    if (( $BAT -le $@ ))
+    if (( $BAT < $1 ))
     then
         notify-send -u critical "Battery critically low."
-    elif (( $BAT -ge 98 ))
+    elif (( $BAT > 98 ))
     then
         notify-send -u critical "Battery completely charged."
     fi
