a83c2fc3af5af1455b05ebae2761bb6a5e8db3aa
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-11-22 14:08:30 -0600

    Corrected julia startup process...

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -1,2 +1,14 @@
-using OhMyREPL
-using Revise
+atreplinit() do repl
+    try
+        @eval using Pkg
+        haskey(Pkg.installed(), "OhMyREPL") || @eval Pkg.add("OhMyREPL")
+        haskey(Pkg.installed(), "Revise") || @eval Pkg.add("Revise")
+    catch
+    end
+    try
+        @eval using Revise
+        @eval using OhMyREPL
+        @async Revise.wait_steal_repl_backend()
+    catch
+    end
+end
