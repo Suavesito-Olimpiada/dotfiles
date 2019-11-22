5be1a9ff013173081c8ce80844d61c27c50fe172
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-11-22 14:10:01 -0600

    Still cannot find what I'm looking for...

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
 
-SOUND=$(pactl list sinks short | awk '{printf "%s %s\n", $7, $2}' | sort -r|
+SOUND=$(pactl list sinks short | awk '{printf "%s %s\n", $7, $2}' | sort |
         head -1 | awk '{printf "%s\n", $2}')
 pactl set-sink-volume $SOUND $@
