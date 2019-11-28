b276858f4f0fd37fbc2034338f5a47f7823c7394
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2019-11-28 04:07:14 -0600

    Make pacmanup run commands even if the one before has failed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -59,7 +59,7 @@ alias npmup="npm i -g npm"
 alias flatup="sudo flatpak update"
 alias snapup="sudo snap refresh"
 
-alias pacmanup="yay -Syyuu && sudo mandb && sudo pkgfile --update"
+alias pacmanup="yay -Syyuu; sudo mandb; sudo pkgfile --update"
 alias pacmanclean='sudo pacman -Rns $(pacman -Qtdq)'
 alias paccacheclean='sudo paccache -rvk3'
 alias sysclean='pacmanclean; paccacheclean'
