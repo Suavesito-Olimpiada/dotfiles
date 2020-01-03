86e87fb50d9698122396e6b24df15e7282942f02
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2020-01-02 23:14:23 -0600

    Just changed hdmi.sh to hdmi/hdmi.sh

---
+++ scripts/hdmi/hdmi.sh
@@ -0,0 +1,16 @@
+#!/bin/env sh
+
+SOURCE=`cat /home/jose/.config/scripts/hdmi.st`
+
+if [[ "$SOURCE" = "hdmi" ]]
+then
+    xrandr --output HDMI-1 --mode 1920x1080 --right-of eDP-1 --auto
+    herbstclient detect_monitors
+    herbstclient reload
+    echo screen > /home/jose/.config/scripts/hdmi.st
+else
+    xrandr --output HDMI-1 --mode 1920x1080 --same-as eDP-1
+    herbstclient detect_monitors
+    herbstclient reload
+    echo hdmi > /home/jose/.config/scripts/hdmi.st
+fi
