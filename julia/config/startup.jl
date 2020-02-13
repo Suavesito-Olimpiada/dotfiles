1136274269fe21cd027c665f7c532cfe35eb7261
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2020-02-13 17:00:41 -0600

    Updated julia startup file

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -8,6 +8,9 @@ atreplinit() do repl
         haskey(Pkg.installed(), "ProgressMeter") || @eval Pkg.add("ProgressMeter")
         haskey(Pkg.installed(), "BenchmarkTools") || @eval Pkg.add("BenchmarkTools")
         haskey(Pkg.installed(), "ProfileView") || @eval Pkg.add("ProfileView")
+        haskey(Pkg.installed(), "LanguageServer") || @eval Pkg.add("LanguageServer")
+        haskey(Pkg.installed(), "SymbolServer") || @eval Pkg.add("SymbolServer")
+        haskey(Pkg.installed(), "StaticLint") || @eval Pkg.add("StaticLint")
     catch
     end
     try
@@ -17,6 +20,9 @@ atreplinit() do repl
         @eval using ProgressMeter
         @eval using BenchmarkTools
         @eval using ProfileView
+        @eval using LanguageServer
+        @eval using SymbolServer
+        @eval using StaticLint
         @eval using Revise
         @async Revise.wait_steal_repl_backend()
     catch
