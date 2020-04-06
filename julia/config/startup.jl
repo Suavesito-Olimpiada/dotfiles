ba1a443d0b1c627bb3a6f3fde85c56f0e6707ba5
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2020-04-06 16:26:30 -0500

    More Julia things.

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -1,30 +1,34 @@
 atreplinit() do repl
     try
-        @eval using Pkg
-        haskey(Pkg.installed(), "OhMyREPL") || @eval Pkg.add("OhMyREPL")
-        haskey(Pkg.installed(), "Revise") || @eval Pkg.add("Revise")
-        haskey(Pkg.installed(), "Traceur") || @eval Pkg.add("Traceur")
-        haskey(Pkg.installed(), "Rebugger") || @eval Pkg.add("Rebugger")
-        haskey(Pkg.installed(), "ProgressMeter") || @eval Pkg.add("ProgressMeter")
-        haskey(Pkg.installed(), "BenchmarkTools") || @eval Pkg.add("BenchmarkTools")
-        haskey(Pkg.installed(), "ProfileView") || @eval Pkg.add("ProfileView")
-        haskey(Pkg.installed(), "LanguageServer") || @eval Pkg.add("LanguageServer")
-        haskey(Pkg.installed(), "SymbolServer") || @eval Pkg.add("SymbolServer")
-        haskey(Pkg.installed(), "StaticLint") || @eval Pkg.add("StaticLint")
-    catch
-    end
-    try
         @eval using OhMyREPL
         @eval using Traceur
         @eval using Rebugger
+        @eval using Debugger
         @eval using ProgressMeter
         @eval using BenchmarkTools
-        @eval using ProfileView
-        @eval using LanguageServer
-        @eval using SymbolServer
-        @eval using StaticLint
+        # @eval using ProfileView
         @eval using Revise
         @async Revise.wait_steal_repl_backend()
-    catch
+    catch e
+        @warn(e.msg)
     end
 end
+
+#=
+try
+    @eval using Pkg
+    haskey(Pkg.installed(), "OhMyREPL") || @eval Pkg.add("OhMyREPL")
+    haskey(Pkg.installed(), "Revise") || @eval Pkg.add("Revise")
+    haskey(Pkg.installed(), "Traceur") || @eval Pkg.add("Traceur")
+    haskey(Pkg.installed(), "Rebugger") || @eval Pkg.add("Rebugger")
+    haskey(Pkg.installed(), "Debugger") || @eval Pkg.add("Debugger")
+    haskey(Pkg.installed(), "ProgressMeter") || @eval Pkg.add("ProgressMeter")
+    haskey(Pkg.installed(), "BenchmarkTools") || @eval Pkg.add("BenchmarkTools")
+    # haskey(Pkg.installed(), "ProfileView") || @eval Pkg.add("ProfileView")
+    haskey(Pkg.installed(), "LanguageServer") || @eval Pkg.add("LanguageServer")
+    haskey(Pkg.installed(), "SymbolServer") || @eval Pkg.add("SymbolServer")
+    haskey(Pkg.installed(), "StaticLint") || @eval Pkg.add("StaticLint")
+    haskey(Pkg.installed(), "JuliaFormatter") || @eval Pkg.add("StaticLint")
+catch
+end
+=#
