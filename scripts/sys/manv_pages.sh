462bb295d6a711dbdb4b439a5a04af1cf4e62efd
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2023-02-07 23:40:45 -0600

    change man pages in vim

--- scripts/sys/manv_pages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DMENU="$HOME/.config/scripts/lib/dmenu.sh"
-search=$(man -k . | $DMENU -i -l 11 -p "Man Page: " | awk '{print $2 " " $1}' | sed 's/(\(.*\))\(.*\)/\1\2/')
-if [[ -n $search ]]
-then
-    alacritty -e bash -ci "man $search | env MAN_PN=1 vim -M +MANPAGER -"
-fi
